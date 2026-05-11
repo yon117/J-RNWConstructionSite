81cad916ec95563697a49a207cdf18980be93727
ElToy\Diego Ramirez <yon117228@gmail.com>
2026-05-11 10:02:49 -0600

    Add TypeScript declaration for @typebot.io/react

---
+++ types/typebot.d.ts
@@ -0,0 +1,25 @@
+declare module '@typebot.io/react' {
+  import { FC } from 'react';
+
+  interface BubbleThemeButton {
+    backgroundColor?: string;
+    customIconSrc?: string;
+  }
+
+  interface BubbleTheme {
+    button?: BubbleThemeButton;
+  }
+
+  interface BubblePreviewMessage {
+    message: string;
+  }
+
+  interface BubbleProps {
+    typebot: string;
+    apiHost?: string;
+    previewMessage?: BubblePreviewMessage;
+    theme?: BubbleTheme;
+  }
+
+  export const Bubble: FC<BubbleProps>;
+}
